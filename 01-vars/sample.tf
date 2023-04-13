841219cd94acef66ae9ff52e1d848e7f4c742933
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-13 15:51:13 +0530

    Auto Push

--- 01-vars/sample.tf
+++ 01-vars/sample.tf
@@ -5,3 +5,9 @@ variable "sample" {
 output "sample_op" {
     value = var.sample
 }
+
+output "sample_outpiut" {
+    value = "Value of the sample variable is ${var.sample}"
+}
+
+# A variable cann be accessed without ${} as well , if its not accompanied in any sentence. If you just want to use it, you can directly refer it as `var.VariableName`
