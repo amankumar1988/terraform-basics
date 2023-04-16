c5b36a11c31fac0967831cb3f31699599b6a5a93
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-16 13:02:45 +0530

    Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -19,3 +19,30 @@ resource "aws_instance" "web" {
 output "private_dns_of_Server" {
     value = aws_instance.web.private_dns
 }
+
+# Create security group
+
+resource "aws_security_group" "allow_ssh" {
+  name        = "allow_ssh"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = aws_vpc.main.id
+
+  ingress {
+    description      = "SSH from VPC"
+    from_port        = 22
+    to_port          = 2
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_ssh"
+  }
+}
