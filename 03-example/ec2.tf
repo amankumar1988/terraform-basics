32b9f8230e37680aded06e80624883e18f4370b0
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-16 12:44:09 +0530

    Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -1,6 +1,15 @@
-# Always start with declaring information first
+# Always start with declaring provider information first
 
 provider "aws" {
     region = "us-east-1"
 }
 
+resource "aws_instance" "web" {
+  ami                       = "ami-007fbb30bb8ade45b"
+  instance_type             = "t3.nano"
+  vpc_security_group_ids    = [aws_security_group.allow_ssh_sg.id]
+
+  tags = {
+    Name        = "Terraform-Instance-Server-Name"
+  }
+}
