5f60ded6108b0de57796a59b739084618e1008e2
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-16 13:11:46 +0530

    Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -1,9 +1,3 @@
-# Always start with declaring provider information first
-
-provider "aws" {
-    region = "us-east-1"
-}
-
 # Create EC2
 resource "aws_instance" "web" {
   ami                       = "ami-007fbb30bb8ade45b"
---
+++ 03-example/provider.tf
@@ -0,0 +1,5 @@
+# Always start with declaring provider information first
+
+provider "aws" {
+    region = "us-east-1"
+}
