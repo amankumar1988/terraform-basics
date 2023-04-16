094f7cbb1749a428850f9ff4fefaa653cf4866bd
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-16 14:14:29 +0530

    Auto Push

---
+++ 04-examples/backend.tf
@@ -0,0 +1,15 @@
+# Always start with declaring provider information first
+
+provider "aws" {
+    region = "us-east-1"
+}
+
+# Declaring the remote backend; This will keep the state files in a remote s3 buckets and this will let team collarboration
+
+terraform {
+  backend "s3" {
+    bucket = "b53-tfstate-bucket"
+    key    = "modules/tf/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
---
+++ 04-examples/ec2/main.tf
@@ -0,0 +1,16 @@
+# Create EC2
+resource "aws_instance" "web" {
+  ami                       = "ami-007fbb30bb8ade45b"
+  instance_type             = "t3.micro"
+#   vpc_security_group_ids    = [aws_security_group.allow_ssh.id]
+
+  tags = {
+    Name        = "Terraform-Instance-Server-Name-2"
+  }
+}
+
+# Print attribute
+
+# output "private_dns_of_Server" {
+#     value = aws_instance.web.private_dns
+# }
