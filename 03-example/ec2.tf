32eec70fa287671d996b9f120fe05be5241a0a1b
amankumar1988 <kumar.aman6600@gmail.com>
2023-04-16 12:50:52 +0530

    Auto Push

--- 03-example/ec2.tf
+++ 03-example/ec2.tf
@@ -4,11 +4,18 @@ provider "aws" {
     region = "us-east-1"
 }
 
+# Create EC2
 resource "aws_instance" "web" {
   ami                       = "ami-007fbb30bb8ade45b"
   instance_type             = "t3.nano"
-  
+
   tags = {
     Name        = "Terraform-Instance-Server-Name"
   }
+}
+
+# Print attribute
+
+output "private_dns_of_Server" {
+    value = aws_instance.web.private_dns
 }
